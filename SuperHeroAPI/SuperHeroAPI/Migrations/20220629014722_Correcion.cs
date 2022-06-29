db0fa02a9ede9dabdd14d4402e9aac701103c79d
Juan Felipe <juanfecanoco01@gmail.com>
2022-06-29 17:17:55 -0500

    Primer commit del proyecto

---
+++ SuperHeroAPI/SuperHeroAPI/Migrations/20220629014722_Correcion.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SuperHeroAPI.Migrations
+{
+    public partial class Correcion : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "LastaName",
+                table: "SuperHeroes",
+                newName: "LastName");
+
+            migrationBuilder.RenameColumn(
+                name: "FirstaName",
+                table: "SuperHeroes",
+                newName: "FirstName");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "LastName",
+                table: "SuperHeroes",
+                newName: "LastaName");
+
+            migrationBuilder.RenameColumn(
+                name: "FirstName",
+                table: "SuperHeroes",
+                newName: "FirstaName");
+        }
+    }
+}
